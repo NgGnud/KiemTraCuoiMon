3844fff9387aaa609988a22f13bc83112bcfbb24
NgGnud <163308193+NgGnud@users.noreply.github.com>
2025-03-01 14:05:43 +0700

    hihi

---
+++ src/main/java/org/example/kiemtracuoimontest/KiemTraCuoiMonTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.kiemtracuoimontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiemTraCuoiMonTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiemTraCuoiMonTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kiemtracuoimontest/KiemTraCuoiMonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kiemtracuoimontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiemTraCuoiMonTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
